06cc267b8725b37ee4ab1a99ab784e65a500d36c
Dave Cheney <dave@cheney.net>
2013-04-21 21:48:22 +1000

    move future to build.go

--- build.go
+++ build.go
@@ -6,6 +6,33 @@ import (
 	"path/filepath"
 )
 
+// A Future represents some work to be performed.
+type Future interface {
+	// Result returns the result of the work as an error, or nil if the work
+	// was performed successfully.
+	// Implementers must observe these invariants
+	// 1. There may be multiple concurrent callers to Result, or Result may
+	//     be called many times in sequence, it must always return the same
+	//     value.
+	// 2. Result blocks until the work has been performed.
+	Result() error
+}
+
+type future struct {
+	err chan error
+}
+
+func (f *future) Result() error {
+	result := <-f.err
+	f.err <- result
+	return result
+}
+
+// from $GOROOT/src/pkg/go/build/syslist.go
+
+const goosList = "darwin freebsd linux netbsd openbsd plan9 windows "
+const goarchList = "386 amd64 arm "
+
 func Build(pkg *Package) []Future {
 	if pkg.Name() == "main" {
 		return buildCommand(pkg)
--- gogo.go
+++ gogo.go
@@ -6,33 +6,6 @@ import (
 	"unicode"
 )
 
-// A Future represents some work to be performed.
-type Future interface {
-	// Result returns the result of the work as an error, or nil if the work
-	// was performed successfully.
-	// Implementers must observe these invariants
-	// 1. There may be multiple concurrent callers to Result, or Result may
-	//     be called many times in sequence, it must always return the same
-	//     value.
-	// 2. Result blocks until the work has been performed.
-	Result() error
-}
-
-type future struct {
-	err chan error
-}
-
-func (f *future) Result() error {
-	result := <-f.err
-	f.err <- result
-	return result
-}
-
-// from $GOROOT/src/pkg/go/build/syslist.go
-
-const goosList = "darwin freebsd linux netbsd openbsd plan9 windows "
-const goarchList = "386 amd64 arm "
-
 // from $GOROOT/src/pkg/go/build/build.go
 
 // splitQuoted splits the string s around each instance of one or more consecutive
