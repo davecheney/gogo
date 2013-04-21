28efaa5eb8b3145451ce715e4c03e33fd7ce7214
Dave Cheney <dave@cheney.net>
2013-04-21 15:08:33 +1000

    go vet

--- package.go
+++ package.go
@@ -173,9 +173,9 @@ func (p *Package) scanFiles(files []os.FileInfo) error {
 						}
 						switch path {
 						case "":
-							return fmt.Errorf("package %q imported blank path: %s", spec.Pos())
+							return fmt.Errorf("package %q imported blank path: %s", p.Name(), spec.Pos())
 						case ".":
-							return fmt.Errorf("package %q imported dot path: %s", spec.Pos())
+							return fmt.Errorf("package %q imported dot path: %s", p.Name(), spec.Pos())
 						case "C":
 							if isTest {
 								return fmt.Errorf("use of cgo in test %s not supported", filename)
